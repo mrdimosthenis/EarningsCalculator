4a84d2ede01b988992909303b95d3c5d37d75245
mrdimosthenis <dimos86m@gmail.com>
2023-10-27 11:27:15 +0300

    Basic implementation

--- Program.fs
+++ Program.fs
@@ -1,2 +1,15 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+open System.Globalization
+
+[<EntryPoint>]
+let main args =
+    match args with
+    | [| monthArg; hoursArg; rateArg |] ->
+        DateTime.ParseExact(monthArg, "yyyy-MM", null, DateTimeStyles.None) |> ignore
+        let hours = hoursArg |> int |> decimal
+        let rate = Decimal.Parse(rateArg)
+        Console.WriteLine $"Your earnings for {monthArg} are {hours * rate}"
+        0
+    | _ ->
+        Console.WriteLine "Please provide three arguments"
+        1
